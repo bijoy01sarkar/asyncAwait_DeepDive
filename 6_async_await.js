b78117327cc60d739b2a85cfb8762374861ad5e1
Bijoy Sarkar <75505462+bijoy01sarkar@users.noreply.github.com>
2025-04-11 12:12:33 +0530

    Update 6_async_await.js

--- 6_async_await.js
+++ 6_async_await.js
@@ -1,7 +1,8 @@
-// # 6️⃣ Timeout with Async/Await
-// Write a function `wait(ms)` that returns a promise that resolves after `ms` milliseconds.  
-// Use `async/await` to pause execution for 5 seconds before logging `"Done waiting!"`.
-// // 6️⃣ Timeout with Async/Await
+// 6. Timeout with Async/Await
+// Write a function wait(ms) that returns a promise that resolves after 'ms' milliseconds.  
+// Use async/await to pause execution for 5 seconds before logging "Done waiting!".
+// 6. Timeout with Async/Await
+
 function wait(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
