45909f12f534d4510213662a4cc66a8b932089c7
Bijoy Sarkar <75505462+bijoy01sarkar@users.noreply.github.com>
2025-04-11 11:47:36 +0530

    Update 3_async_await.js

--- 3_async_await.js
+++ 3_async_await.js
@@ -1,11 +1,10 @@
-
-
 // 3. Multiple Promises (Promise.all)
 // Write two functions:
-// 1. `fetchPosts()` → Returns a promise that resolves to `["Post 1", "Post 2", "Post 3"]` after 2 seconds.
-// 2. `fetchComments()` → Returns a promise that resolves to `["Comment A", "Comment B", "Comment C"]` after 3 seconds.
-// Use `Promise.all()` to fetch both posts and comments together and print them.
-// // 3️⃣ Multiple Promises (Promise.all)
+// 1. fetchPosts() → Returns a promise that resolves to ["Post 1", "Post 2", "Post 3"] after 2 seconds.
+// 2. fetchComments() → Returns a promise that resolves to ["Comment A", "Comment B", "Comment C"] after 3 seconds.
+// Use Promise.all() to fetch both posts and comments together and print them.
+
+// 3. Multiple Promises (Promise.all)
 function fetchPosts() {
     return new Promise((resolve) => {
         setTimeout(() => resolve(["Post 1", "Post 2", "Post 3"]), 2000);
