e98b72b14a3c169b551eda31912a0b8bc37e567b
Bijoy Sarkar <75505462+bijoy01sarkar@users.noreply.github.com>
2025-04-11 12:20:21 +0530

    Update 7_async_await.js

--- 7_async_await.js
+++ 7_async_await.js
@@ -1,6 +1,9 @@
-// # 7️⃣ Fetching Multiple Users (Async/Await Loop)
-// Create an array of user IDs: `[1, 2, 3]`. Write an `async function fetchAllUsers(userIds)` that loops through each user ID and fetches user data one-by-one using `fetchUserData(userId)` (from assignment 2). Log each user's data after fetching.
-// // 7️⃣ Fetching Multiple Users (Async/Await Loop)
+// 7. Fetching Multiple Users (Async/Await Loop)
+// Create an array of user IDs: [1, 2, 3]. Write an 'async function fetchAllUsers(userIds) that 
+// loops through each user ID and fetches user data one-by-one using 'fetchUserData(userId)' (from assignment 2).
+// Log each user's data after fetching.
+// 7. Fetching Multiple Users (Async/Await Loop)
+
 const userIds = [1, 2, 3];
 async function fetchAllUsers(userIds) {
     for (const id of userIds) {
