38f52a3421bea9cfee6064826b70a0893e28fd53
Bijoy Sarkar <75505462+bijoy01sarkar@users.noreply.github.com>
2025-04-11 11:56:47 +0530

    Update 4_async_await.js

--- 4_async_await.js
+++ 4_async_await.js
@@ -1,8 +1,8 @@
-
-// # 4️⃣ Handling Promise Rejection (try/catch)
-// Write a function `fetchData(url)` that simulates a network request. If the URL is `"valid.com"`, resolve with `"Data received"` after 2 seconds. Otherwise, reject with `"Invalid URL"`.  
-// Use `async/await` and handle errors with `try/catch`.
-// 4️⃣ Handling Promise Rejection (try/catch)
+// 4. Handling Promise Rejection (try/catch)
+// Write a function fetchData(url) that simulates a network request. 
+// If the URL is "valid.com", resolve with "Data received" after 2 seconds. Otherwise, reject with "Invalid URL".  
+// Use async/await and handle errors with try/catch.
+// 4. Handling Promise Rejection (try/catch)
 function fetchData(url) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
